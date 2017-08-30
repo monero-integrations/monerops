e45be692d61650d4e4ca61a5c4ce1807ff7b6324
serhack <atombt@protonmail.com>
2017-08-30 14:46:30 +0200

    Update monero.php

--- modules/monero/monero.php
+++ modules/monero/monero.php
@@ -5,76 +5,79 @@
  *      support@monerointegrations.com
  */
 
+class monero extends PaymentModule{
 
-class monero extends PaymentModule
-{
-    private $_html = '';
-    private $_postErrors = array();
+        private $_html = '';
+        private $_postErrors = array();
         
-    public function __construct()
-    {
-        $this->name = "monero";
-        $this->tab = 'payments_gateways';
-        $this->version = '0.1.0';
-        $this->author = 'SerHack';
-        $this->need_instance = 1;
-        $this->bootstrap = true;
+        function __construct(){
+        
+                $this->name = "monero";
+                $this->tab = 'payments_gateways';
+                $this->version = '0.1.2';
+                $this->author = 'SerHack';
+                $this->need_instance = 1;
+                $this->bootstrap = true;
             
-        parent::__construct();
+                parent::__construct();
                 
-        $this->displayName = $this->l('Monero Payments');
-        $this->description = $this->l('Module for accepting payments by Monero');
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-    }
-    
-    public function install()
-    {
-        if (!function_exists('curl_version')) {
-            $this->_errors[] = $this->l('Sorry, this module requires the cURL PHP extension but it is not enabled on your server.  Please ask your web hosting provider for assistance.');
-            return false;
+                $this->displayName = $this->l('Monero Payments');
+                $this->description = $this->l('Module for accepting payments by Monero');
+                $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
         }
+    
+         public function install(){
              
-        if (!parent::install()
+             if(!function_exists('curl_version')) {
+                $this->_errors[] = $this->l('Sorry, this module requires the cURL PHP extension but it is not enabled on your server.  Please ask your web hosting provider for assistance.');
+                 return false;
+             }
+             
+             if (!parent::install()
                  or !$this->registerHook('payment')
                  or !$this->registerHook('paymentReturn')
                  or !$this->registerHook('displayPDFInvoice')
                  or !$this->registerHook('invoice')
+                 or  !$this->registerHook('header')
+          
                 ) {
-            return false;
+                return false;
+             }
+             $this->active = true;
+             return true;
         }
-        $this->active = true;
-        return true;
-    }
     
-    public function getContent()
-    {
-        $output = null;
+        public function getContent() {
+	$output = null;
 
-        if (Tools::isSubmit('submit'.$this->name)) {
-            $my_module_name = strval(Tools::getValue('MONERO_ADDRESS'));
-            $monero_wallet = strval(Tools::getvalue('MONERO_WALLET'));
-            if (!$my_module_name
-          || empty($my_module_name)
-          || !Validate::isGenericName($my_module_name)) {
-                $output .= $this->displayError($this->l('Invalid Configuration value'));
-            } else {
-                Configuration::updateValue('MONERO_ADDRESS', $my_module_name);
-                Configuration::updateValue('MONERO_WALLET', $monero_wallet);
-                $output .= $this->displayConfirmation($this->l('Settings updated'));
-            }
+            if (Tools::isSubmit('submit'.$this->name))
+    {
+        $monero_address = strval(Tools::getValue('MONERO_ADDRESS'));
+        $monero_wallet = strval(Tools::getvalue('MONERO_WALLET'));
+        if (!$monero_address
+          || empty($monero_address)
+          || !Validate::isGenericName($monero_address))
+            $output .= $this->displayError($this->l('Invalid Configuration value'));
+        else
+        {
+            Configuration::updateValue('MONERO_ADDRESS', $monero_address);
+            Configuration::updateValue('MONERO_WALLET', $monero_wallet);
+            $output .= $this->displayConfirmation($this->l('Settings updated'));
         }
-        return $output.$this->displayForm();
     }
+    return $output.$this->displayForm();
+        }
     
          
 
-    public function displayForm()
-    {
-        // Get default language
-        $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
+public function displayForm()
+{
+    
+    $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
      
-        // Init Fields form array
-        $fields_form[0]['form'] = array(
+    // Init Fields form array
+    $fields_form[0]['form'] = array(
         'legend' => array(
             'title' => $this->l('Settings'),
         ),
@@ -86,11 +89,11 @@ class monero extends PaymentModule
                 'size' => 20,
                 'required' => true
             ),
-        array(
-                'type' => 'text',
-                'label' => $this->l('Monero Wallet RPC IP'),
-                'name' => 'MONERO_WALLET',
-                'required' => false
+            array(
+            	'type' => 'text',
+            	'label' => $this->l('Monero Wallet RPC IP'),
+            	'name' => 'MONERO_WALLET',
+            	'required' => false
             )
         ),
         'submit' => array(
@@ -99,24 +102,24 @@ class monero extends PaymentModule
         )
     );
      
-        $helper = new HelperForm();
+    $helper = new HelperForm();
      
-        // Module, token and currentIndex
-        $helper->module = $this;
-        $helper->name_controller = $this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+    // Module, token and currentIndex
+    $helper->module = $this;
+    $helper->name_controller = $this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminModules');
+    $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
      
-        // Language
-        $helper->default_form_language = $default_lang;
-        $helper->allow_employee_form_lang = $default_lang;
+    // Language
+    $helper->default_form_language = $default_lang;
+    $helper->allow_employee_form_lang = $default_lang;
      
-        // Title and toolbar
-        $helper->title = $this->displayName;
-        $helper->show_toolbar = true;        // false -> remove toolbar
+    // Title and toolbar
+    $helper->title = $this->displayName;
+    $helper->show_toolbar = true;        // false -> remove toolbar
     $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
     $helper->submit_action = 'submit'.$this->name;
-        $helper->toolbar_btn = array(
+    $helper->toolbar_btn = array(
         'save' =>
         array(
             'desc' => $this->l('Save'),
@@ -129,16 +132,15 @@ class monero extends PaymentModule
         )
     );
      
-        // Load current value
-        $helper->fields_value['MONERO_ADDRESS'] = Configuration::get('MONERO_ADDRESS');
-        $helper->fields_value['MONERO_WALLET'] = Configuration::get('MONERO_WALLET');
+    // Load current value
+    $helper->fields_value['MONERO_ADDRESS'] = Configuration::get('MONERO_ADDRESS');
+    $helper->fields_value['MONERO_WALLET'] = Configuration::get('MONERO_WALLET');
      
-        return $helper->generateForm($fields_form);
-    }
+    return $helper->generateForm($fields_form);
+}
 
-    public function hookpayment($params)
-    {
-        $this->smarty->assign(
+public function hookpayment($params){
+$this->smarty->assign(
             array(
             'this_path' => $this->_path,
             'this_path_ssl' => Tools::getHttpHost(true, true) . __PS_BASE_URI__ . 'modules/' . $this->name . '/',
@@ -146,5 +148,12 @@ class monero extends PaymentModule
             )
         );
         return $this->display(__FILE__, 'payment.tpl');
-    }
 }
+ 
+ public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+
+
